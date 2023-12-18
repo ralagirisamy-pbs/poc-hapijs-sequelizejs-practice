793e6b1f6926752c8ca9a5da0de4301d8c14c4ba
ralagirisamy-pbs <ralagirisamy@pbs.org>
2023-12-18 18:28:28 +0530

    Refactor index.js Add startServer and stopServer fns

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const hapi = require("@hapi/hapi");
+const { DEFAULT_HOST, DEFAULT_PORT } = require("./src/_data/constants");
 
-// constants: Uses this if env value is unavailable.
-const DEFAULT_PORT = 3000;
-const DEFAULT_HOST = "localhost";
+let server;
 
-(async function () {
-	// Creating Hapi Server instance
-	const server = hapi.server({
-		port: process.env.PORT || DEFAULT_PORT,
-		host: process.env.HOST || DEFAULT_HOST
-	});
-	// Registering the required plugins for oute and lifecycle hooks.
-	await server.register([require("./src/routes/employee"), require("./src/lib/hooks")]);
-	// Starting the server.
-	await server.start();
-	console.info(`Server started on ${JSON.stringify(server.info.uri)}`);
-})();
+const startServer = async () => {
+	try {
+		// Creating Hapi Server instance
+		server = hapi.server({
+			port: process.env.PORT || DEFAULT_PORT,
+			host: process.env.HOST || DEFAULT_HOST
+		});
+		// Registering the required plugins for oute and lifecycle hooks.
+		await server.register([require("./src/routes/employee"), require("./src/lib/hooks")]);
+		// Starting the server.
+		await server.start();
+		console.info(`Server started on ${JSON.stringify(server.info.uri)}`);
+	} catch (error) {
+		console.error("Error while starting Hapi server: ", error.message);
+		process.exit(1);
+	}
+};
+
+if (process.env.NODE_ENV !== "test") {
+	startServer();
+}
+
+const stopServer = async () => {
+	try {
+		// Wait for 10 secs to shutdown existing connections.
+		await server?.stop({ timeout: 10 * 1000 });
+		process.exit(0);
+	} catch (error) {
+		process.exit(1);
+	}
+};
+
+// Handling terimate signal event
+process.on("SIGINT", stopServer);
+process.on("SIGTERM", stopServer);
---
+++ src/_data/constants.js
@@ -0,0 +1,3 @@
+// constants: Uses this if env value is unavailable.
+exports.DEFAULT_PORT = 3000;
+exports.DEFAULT_HOST = "localhost";
